571e8f142a01d81c806604aad51e9b5bc7818bbb
Ryan Cokely <ryan.cokely@gmail.com>
2023-03-22 14:36:43 -0500

    updating associations

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,16 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Session = require('./models/Session')
 
 //associations could go here!
+Session.belongsTo(User)
+User.hasMany(Session)
 
 module.exports = {
   db,
   models: {
     User,
+    Session
   },
 }
